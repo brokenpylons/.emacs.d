49226447530d637d6fbb8d0caadbc52956e2a714
Žiga Leber <zigaleber7@gmail.com>
2020-01-07 16:45:55 +0100

    Some changes

--- init.el
+++ init.el
@@ -1,3 +1,20 @@
+; Packages
+(require 'package)
+(add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/"))
+(package-initialize)
+(package-refresh-contents)
+
+(setq packages '(ivy-bibtex pdf-tools))
+(print packages)
+
+(dolist (package packages)
+  (unless (package-installed-p package)
+    (package-install package)))
+
+(ivy-mode 1)
+(require 'ivy)
+
+; General settings
 (setq inhibit-splash-screen t)
 ;(setq initial-scratch-message "")
 (menu-bar-mode -1)
@@ -13,56 +30,60 @@
   (interactive "p")
   (beginning-of-line)
   (open-line arg)
-  (indent-according-to-mode))
+  (when newline-and-indent
+    (indent-according-to-mode)))
 (global-set-key (kbd "S-C-o") 'command-open-above)
 
 (defun command-open-bellow (arg)
   (interactive "p")
   (end-of-line)
   (open-line arg)
-  (next-line)
-  (indent-according-to-mode))
+  (next-line 1)
+  (when newline-and-indent
+    (indent-according-to-mode)))
 (global-set-key (kbd "C-o") 'command-open-bellow)
 
 (defun command-join ()
   (interactive)
-  (save-excursion
-    (next-line)
-    (join-line)))
+  (next-line)
+  (join-line))
 (global-set-key (kbd "C-j") 'command-join)
 
-(define-key key-translation-map (kbd "ESC") (kbd "C-g"))
+(add-hook 'minibuffer-setup-hook
+          (lambda ()
+            (local-set-key [escape] 'minibuffer-keyboard-quit)))
+(define-key query-replace-map [escape] 'quit)
+(global-set-key [escape] 'keyboard-quit)
 
 (global-unset-key (kbd "<M-left>"))
 (global-unset-key (kbd "<M-right>"))
 (global-unset-key (kbd "<M-up>"))
 (global-unset-key (kbd "<M-down>"))
 
+(setq-default indent-tabs-mode nil)
 (setq backup-directory-alist '(("." . "~/.emacs.d/backup/")))
 
-; Packages
-(require 'package)
-(add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/"))
-(package-initialize)
-(package-refresh-contents)
-
-(setq packages '(ivy-bibtex))
-(print packages)
-
-(dolist (package packages)
-  (unless (package-installed-p package)
-    (package-install package)))
-
-(require 'ivy)
-(ivy-mode 1)
-
-(require 'ido)
-(ido-mode t)
-
-;; (require 'org-ref)
 (setq bibliography-path "~/Mind/bibliography/")
-;; (setq org-ref-bibliography-notes (concat bibliography-path "notes.org")
-;;       org-ref-default-bibliography (list (concat bibliography-path "references.bib"))
-;;       org-ref-pdf-directory (concat bibliography-path "files/"))
+(setq bibtex-completion-library-path (list (concat bibliography-path "files/"))
+      bibtex-completion-bibliography (list (concat bibliography-path "references.bib")))
 
-(setq bibtex-completion-bibliography (list (concat bibliography-path "references.bib")))
+(setq org-ellipsis "…")
+
+;; (require 'openwith)
+;; (openwith-mode t)
+;; (setq openwith-associations '(("\\.pdf\\'" "evince" (file))))
+
+(setq-default cursor-type 'bar)
+
+(defalias 'doc-view-mode 'fundamental-mode)
+
+(require 'bibtex)
+(add-hook 'bibtex-mode-hook
+          (lambda () (setq fill-column 100)))
+
+(setq bibtex-autokey-edit-before-use nil)
+(setq bibtex-autokey-year-title-separator ":")
+(setq bibtex-autokey-titlewords 1)
+(setq bibtex-autokey-titleword-length nil)
+
+(setq js-indent-level 2)
